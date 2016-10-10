0852ab6d7db144c25d5ff65a53d18c167880603f
javierale84 <javiermale@gmail.com>
2016-10-10 12:24:03 -0300

    tp2

---
+++ Tp2/Clase_12_Library/Moto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+
+
+namespace Clase_12_Library
+{
+ public    class Moto:Vehiculo
+    {
+        public Moto( EMarca marca, string patente, ConsoleColor color)
+            : base(marca, patente, color)
+        {
+        }
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("RUEDAS : "+ this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
